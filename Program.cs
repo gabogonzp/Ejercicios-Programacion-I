46e32aac3ac67d6a36565f6652200543752b5d92
Gabriel Gonzalez <39322656+BerryMerry@users.noreply.github.com>
2023-01-16 17:46:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int a, b;
+            int suma = 0;
+
+
+            Console.Write("Introduzca numero de dos digitos:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+
+            a = numero % 10;
+            b = numero / 10;
+            suma = a + b;
+
+            Console.WriteLine(suma);
+            Console.ReadKey();
+
+
+        }
+    }
+}
