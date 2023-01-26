c0d898532f0c614b87f8513e851dc5930c263314
Gabriel Gonzalez <39322656+BerryMerry@users.noreply.github.com>
2023-01-26 16:26:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca diez numeros enteros");
+            int[] array = new int[10];
+            int[] arrayprimeros = new int[10];
+            ;
+
+            int j=0;
+
+            for ( int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.In.ReadLine());
+            }
+
+
+            for ( int i = 0; i < array.Length; i++)
+            {
+                int firstdigit = (int)(array[i] / Math.Pow(10, (int)Math.Log10(array[i])));
+                if (isPrime(firstdigit))
+                {
+                    j++;
+                }
+            }
+
+            Console.WriteLine("Hay {0} primeros digitos primos en el arreglo", j);
+
+            
+
+             bool isPrime(int n)
+            {
+                if (n < 2)
+                {
+                    return false;
+                }
+
+                for (int i=2; i<=Math.Sqrt(n); i++)
+                {
+                    if (n%i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+
+            Console.WriteLine(arrayprimeros[j]);
+            Console.ReadKey();
+        }
+
+
+    }
+}
