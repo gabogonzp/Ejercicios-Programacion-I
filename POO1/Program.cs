4d5bfa100a0ae3dced475e3fedc1126ca8449179
Gabriel Gonzalez <39322656+BerryMerry@users.noreply.github.com>
2023-03-02 00:04:17 -0400

    Add files via upload

--- POO1/Program.cs
+++ POO1/Program.cs
@@ -10,11 +10,24 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Garaje garaje = new Garaje(1);
-                
-            Coche a = new Coche("Toyota", "Highlander");
-            Coche b = new Coche("Hyundai", "Cantus");
+            Garaje garaje = new Garaje();
 
+            
+            Coche coche1 = new Coche("Toyota", "Highlander");
+            Coche coche2 = new Coche("Hyundai", "Cantus");
+
+            Garaje.aceptarCoche(coche2);
+            Garaje.moverCoche(coche2);
+            Garaje.aceptarCoche(coche1);
+            Garaje.moverCoche(coche1);
+            Garaje.aceptarCoche(coche2);
+            Garaje.moverCoche(coche2);
+            Garaje.aceptarCoche(coche1);
+            Garaje.moverCoche(coche1);
+
+
+            Console.ReadKey();
+           
 
             
 
