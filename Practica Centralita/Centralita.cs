50c2b168f5c0ff182af20ccb3645f7567a6f8e88
Gabriel Gonzalez <39322656+BerryMerry@users.noreply.github.com>
2023-03-14 21:29:37 -0400

    Add files via upload

---
+++ Practica Centralita/Centralita.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCentralita
+{
+    
+        public class Centralita : Llamada
+        {
+            public List<(string, string)> Lista { get; set; }
+            public Centralita(string numOrigen, string numDestino) : base(numOrigen, numDestino)
+            {
+                Lista = new List<(string, string)>();
+
+                 void AgregarDatos(List<(string, string)> Lista)
+                {
+
+                     numOrigen = Console.ReadLine();
+                     numDestino = Console.ReadLine();
+                    Lista.Add((numOrigen, numDestino));
+
+                    Console.WriteLine(Lista);
+                }
+
+            }
+        }
+    }
+    
+
