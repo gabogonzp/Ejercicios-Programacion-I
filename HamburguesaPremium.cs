8ac3c0db6b964dfef82b75a1ce08b97437f30f31
Gabriel Gonzalez <39322656+BerryMerry@users.noreply.github.com>
2023-04-03 17:55:57 -0400

    Add files via upload

---
+++ HamburguesaPremium.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChimiDeBilly
+{
+    class HamburguesaPremium : Hamburguesa
+    {
+        public HamburguesaPremium(string tipoCarne, int precioBase) : base("Tradicional", tipoCarne, precioBase  )
+        {
+            AgregarAdicional("Papitas", 2);
+            AgregarAdicional("Bebida", 1);
+        }
+
+        public new void AgregarAdicional(string ingrediente, int precio)
+        {
+            Console.WriteLine("No se permiten ingredientes adicionales en una Hamburguesa Premium");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+
+namespace ChimiDeBilly
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("//Bienvenido al puesto de Chimi de Billy//");
+            Console.WriteLine($"Ofrecemos tres tipos de hamburguesa, cual quiere probar?\n1)Hamburguesa Classic\n2)Hamburguesa Saludable\n3)Hamburguesa Premium\nDigite su opcion:");
+            int _hamchoice = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            if ( _hamchoice == 1 ) 
+            {
+                Console.WriteLine("Sabia eleccion, proceda a seleccionar su preferencia de pan, carne y adicionales.");
+                string carne=string.Empty;
+                string pan = string.Empty;
+
+                Console.WriteLine($"Opciones de pan:\n1)De agua\n2)Sobao");
+                int panchoice = Convert.ToInt32(Console.ReadLine());
+
+                if ( panchoice == 1)
+                {
+                    pan = "De Agua";
+                }
+
+                else if (panchoice == 2)
+                {
+                    pan = "Sobao";
+                }
+
+                Console.WriteLine($"Opciones de carne:\n1)Res\n2)Pollo");
+                int carnechoice = Convert.ToInt32(Console.ReadLine());
+
+                if (carnechoice == 1)
+                {
+                    carne = "Res";
+                }
+
+                else if (carnechoice == 2)
+                {
+                    carne = "Pollo";
+                }
+
+                Hamburguesa hamburguesa = new(pan, carne, 100);
+
+                Console.WriteLine($"Seleccione su ingrediente adicional preferido:\n1)Lechuga\n2)Tomate\n3)Cebolla\n4)Pepinillos");
+                int ingredientechoice = Convert.ToInt32(Console.ReadLine());
+                string ingredichoice;
+
+                
+
+                hamburguesa.SeleccionarIngrediente(ingredientechoice);
+                hamburguesa.CalcularCosto();
+
+               
+
+                Console.WriteLine($"Su tipo de pan:{ pan}, su tipo de carne{ carne}");
+
+
+
+            }
+            if (_hamchoice == 2) { }
+            if (_hamchoice == 3) { }
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
